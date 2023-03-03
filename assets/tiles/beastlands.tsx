91f2b0d24f921ba8cbcefcf604fa085a46e50192
Jason Albino <jason.albino@gruposolutio.com>
2023-03-03 16:38:20 +0100

    Images

---
+++ assets/tiles/beastlands.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="beastlands" tilewidth="32" tileheight="32" tilecount="493" columns="29">
+ <image source="tilesets/beastlands.png" width="944" height="544"/>
+</tileset>
